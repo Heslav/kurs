94ed69c585ec1e081d602ae0a931313cc40b9d24
Heslav <heslav@op.pl>
2023-06-13 16:53:50 +0200

    Made task number 3

---
+++ src/main/java/pl/programming4you/lectures5/lecture1/Task3.java
@@ -0,0 +1,13 @@
+package pl.programming4you.lectures5.lecture1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        int firstNumber = 30;
+        int secondNumber = 15;
+
+        System.out.println(firstNumber + " plus " + secondNumber + " is " + (firstNumber + secondNumber));
+        System.out.println(firstNumber + " minus " + secondNumber + " is " + (firstNumber - secondNumber));
+        System.out.println(firstNumber + " times " + secondNumber + " is " + (firstNumber * secondNumber));
+        System.out.println(firstNumber + " divided by " + secondNumber + " is " + (firstNumber / secondNumber));
+    }
+}
