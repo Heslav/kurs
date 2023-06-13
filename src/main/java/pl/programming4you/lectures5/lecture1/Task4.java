6955029e36cf531d419d81dfe5fa9d666318ad8c
Heslav <heslav@op.pl>
2023-06-13 16:56:25 +0200

    Made task number 4

---
+++ src/main/java/pl/programming4you/lectures5/lecture1/Task4.java
@@ -0,0 +1,13 @@
+package pl.programming4you.lectures5.lecture1;
+
+public class Task4 {
+    public static void main(String[] args) {
+        String randomText = "Let's go Brandon";
+        if (randomText.length() <= 7) {
+            if (randomText.length() <= 7) {
+                System.out.println("Your string is not too long");
+            } else
+                System.out.println("Your string is quite long");
+        }
+    }
+}
