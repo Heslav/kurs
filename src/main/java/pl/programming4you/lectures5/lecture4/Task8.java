423e6182820ece91a5bd1190dff5c998b64d167f
Heslav <heslav@op.pl>
2023-06-17 14:08:18 +0200

    Implemented task 8

---
+++ src/main/java/pl/programming4you/lectures5/lecture4/Task8.java
@@ -0,0 +1,23 @@
+package pl.programming4you.lectures5.lecture4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Task8 {
+    public static boolean haveSameElements(List<String> list, String[] array) {
+        int size = Math.min(list.size(), array.length);
+        for (int i = 0; i < size; i++) {
+            if (!list.get(i).equals(array[i])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>(List.of("a", "b", "c", "d"));
+        String[] array = {"a", "b"};
+        System.out.println(haveSameElements(list, array));
+
+    }
+}
