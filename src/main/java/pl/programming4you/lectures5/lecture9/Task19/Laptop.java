e4e33cd8b8e91e598bd343943e6ee25f5690f883
Heslav <heslav@op.pl>
2023-07-31 12:19:45 +0200

    Implemented task 19, made fields private and final, removed empty lines, override equals and hashCode

--- src/main/java/pl/programming4you/lectures5/lecture9/Task19/Laptop.java
+++ src/main/java/pl/programming4you/lectures5/lecture9/Task19/Laptop.java
@@ -1,8 +1,10 @@
 package pl.programming4you.lectures5.lecture9.Task19;
 
 
+import java.util.Objects;
+
 public class Laptop extends Computer {
-    double batteryCapacityWh;
+    private final double batteryCapacityWh;
 
     public Laptop(int RAMGb, int cores, int baseClockMhz, GraphicsCardType GPU, double batteryCapacityWh) {
         super(RAMGb, cores, baseClockMhz, GPU);
@@ -13,12 +15,26 @@ public class Laptop extends Computer {
         return batteryCapacityWh;
     }
 
+    // return super
     @Override
     public String toString() {
-        return "Laptop: RAM=" + RAMGb + "GB, CPU=" + cores + " cores, Clock Speed=" + baseClockMhz + "MHz, Graphics=" + GPU
-                + ", Battery capacity: " + batteryCapacityWh;
+        return "Laptop: RAM=" + super.getRAMGb() + "GB, CPU=" + super.getCores() + " cores, Clock Speed=" +
+                super.getBaseClockMhz() + "MHz, Graphics=" + super.getGPU() + ", Battery capacity: " +
+                batteryCapacityWh;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), batteryCapacityWh);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Laptop laptop = (Laptop) o;
+        return batteryCapacityWh == laptop.batteryCapacityWh;
     }
 
 }
-
-
