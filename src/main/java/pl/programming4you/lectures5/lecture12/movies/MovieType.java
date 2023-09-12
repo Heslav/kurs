4081cdaadcc9fcffe4f70bff707991e2b9473143
Heslav <heslav@op.pl>
2023-09-12 17:09:41 +0200

    Implemented task 24.

---
+++ src/main/java/pl/programming4you/lectures5/lecture12/movies/MovieType.java
@@ -0,0 +1,5 @@
+package pl.programming4you.lectures5.lecture12.movies;
+
+public enum MovieType {
+    ACTION, ADVENTURE, HORROR, THRILLER, FANTASY, COMEDY, DRAMA
+}
---
+++ src/main/java/pl/programming4you/lectures5/lecture12/server/MyHttpHandler.java
@@ -0,0 +1,86 @@
+package pl.programming4you.lectures5.lecture12.server;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import pl.programming4you.lectures5.lecture12.movies.Movie;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+public class MyHttpHandler implements HttpHandler {
+    private static final Logger logger = Logger.getLogger(MyHttpHandler.class.getName());
+    private final Map<Integer, Movie> movies = new HashMap<>();
+    private final ObjectMapper objectMapper = new ObjectMapper();
+    private int lastId = 0;
+
+
+    @Override
+    public void handle(HttpExchange httpExchange) throws IOException {
+        Movie returnedMovie = null;
+        String errorMessage = null;
+        try {
+            if ("GET".equals(httpExchange.getRequestMethod())) {
+                returnedMovie = handleGetRequest(httpExchange);
+            } else if ("POST".equals(httpExchange.getRequestMethod())) {
+                returnedMovie = handlePostRequest(httpExchange);
+            }
+
+        } catch (CannotProcessJsonException e) {
+            errorMessage = e.getMessage();
+        }
+        handleResponse(httpExchange, returnedMovie, errorMessage);
+    }
+
+    private Movie handlePostRequest(HttpExchange httpExchange) throws CannotProcessJsonException {
+        try {
+            Movie movie;
+            movie = objectMapper.readValue(httpExchange.getRequestBody(), Movie.class);
+            int id = lastId++;
+            Movie movieWithId = new Movie(id, movie.name(), movie.movieType(), movie.releaseYear(), movie.rating());
+            movies.put(id, movieWithId);
+            logger.info("Saved movie " + movieWithId);
+            return movieWithId;
+        } catch (IOException e) {
+            throw new CannotProcessJsonException("Cannot properly process Json content.");
+        }
+
+    }
+
+    private Movie handleGetRequest(HttpExchange httpExchange) {
+        String requestUri = httpExchange.getRequestURI().toString();
+        Integer id = Integer.parseInt(requestUri.substring(requestUri.lastIndexOf('/') + 1));
+        return movies.get(id);
+    }
+
+    private void handleResponse(HttpExchange httpExchange, Movie movie, String errorMessage) throws IOException {
+        OutputStream outputStream = httpExchange.getResponseBody();
+        String response;
+        int responseCode;
+        int responseLength;
+
+        if (errorMessage != null) {
+            responseCode = 400;
+            response = errorMessage;
+            responseLength = response.length();
+        } else {
+            if (movie == null) {
+                responseCode = 404;
+                response = null;
+                responseLength = 0;
+            } else {
+                responseCode = 200;
+                response = objectMapper.writeValueAsString(movie);
+                responseLength = response.length();
+            }
+        }
+        httpExchange.sendResponseHeaders(responseCode, responseLength);
+        if (response != null) {
+            outputStream.write(response.getBytes());
+        }
+        outputStream.flush();
+    }
+}
