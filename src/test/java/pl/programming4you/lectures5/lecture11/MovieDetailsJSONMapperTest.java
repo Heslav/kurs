d3cad0b4b7d36e0e96cbcc68cab5da7065ff554c
Heslav <heslav@op.pl>
2023-09-17 20:50:41 +0200

    Task 23: Corrected dependencies and tests.

--- src/test/java/pl/programming4you/lectures5/lecture11/MovieDetailsJSONMapperTest.java
+++ src/test/java/pl/programming4you/lectures5/lecture11/MovieDetailsJSONMapperTest.java
@@ -1,7 +1,7 @@
 package pl.programming4you.lectures5.lecture11;
 
-import org.junit.Test;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 
